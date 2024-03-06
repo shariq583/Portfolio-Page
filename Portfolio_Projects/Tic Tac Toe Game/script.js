d0855ce7727d5fb35a856b778947b475fc2176cb
shariq583 <gazdarshariq0@gmail.com>
2024-03-06 20:04:15 +0500

    Tic Tac Toe Game all set!

--- Portfolio_Projects/Tic Tac Toe Game/script.js
+++ Portfolio_Projects/Tic Tac Toe Game/script.js
@@ -3,6 +3,9 @@ let gameBtns = document.querySelectorAll(".cell");
 let turnDisp = document.querySelector(".text");
 let winDisp = document.querySelector(".win");
 let resetBtn = document.querySelector("#resetBtn");
+var audio = new Audio("Audio/pop-39222.mp3");
+var sucAudio = new Audio("Audio/success-1-6297.mp3");
+let gameAudio = new Audio("Audio/game-music-loop-7-145285.mp3");
 let winPattern = [
   [0, 1, 2],
   [0, 3, 6],
@@ -14,8 +17,10 @@ let winPattern = [
   [6, 7, 8],
 ];
 let evt;
+gameAudio.play();
 gameBtns.forEach((box) => {
   box.addEventListener("click", () => {
+    audio.play();
     if (turnX == true) {
       box.innerText = "X";
       turnX = false;
@@ -43,6 +48,7 @@ const winCheck = () => {
     let pos3Val = gameBtns[pattern[2]].innerText;
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val == pos2Val && pos2Val == pos3Val) {
+        sucAudio.play();
         winDisp.innerText = `Winner is ${pos3Val}`;
         for (const btn of gameBtns) {
           btn.disabled = true;
